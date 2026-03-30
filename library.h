fa98ddd6eac2d3866d97be224292f899a8c50705
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2026-03-30 11:09:58 -0400

    Create library.h

---
+++ library.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * Base class
+ */ 
+class Item {
+  /*write your code*/
+};
+
+
+/*
+ * Derived class that will represent a book
+ */
+class Book : public Item {
+  /*write your code*/
+};
+
+
+/*
+ * Derived class that will represent a DVD
+ */
+class DVD : public Item {
+  /*write your code*/
+};
